d70624af5eec1b82d2f8cbbda65b3a28603c98d9
henriquecesar139 <henriquepb139@gmail.com>
2021-08-26 13:55:21 -0300

    Otimização de código

--- script.js
+++ script.js
@@ -2,45 +2,24 @@
 
 var erros = 0
 
+elementos1 = [0, 1, 2, 0]
+elementos2 = [3, 4, 5, 1]
+elementos3 = [6, 7, 8, 2]
+elementos4 = [9, 10, 11, 3]
+ultima = "ultima"
 
-function avaliar(resposta) {   
+
+function avaliar(resposta, elemento, pergunta = 0) {   
     if(!resposta) {
         erros = erros + 1
     }
-    document.getElementsByClassName('errada')[0].style = 'background-color: red;'
-    document.getElementsByClassName('errada')[1].style = 'background-color: red;'
-    document.getElementsByClassName('errada')[2].style = 'background-color: red;'
-    document.getElementsByClassName('certa')[0].style = 'background-color: green;'
-}
 
-function avaliar2(resposta) {   
-    if(!resposta) {
-        erros = erros + 1
-    }
-    document.getElementsByClassName('errada2')[0].style = 'background-color: red;'
-    document.getElementsByClassName('errada2')[1].style = 'background-color: red;'
-    document.getElementsByClassName('errada2')[2].style = 'background-color: red;'
-    document.getElementsByClassName('certa2')[0].style = 'background-color: green;'
-}
+    document.getElementsByClassName('errada')[elemento[0]].style = 'background-color: red;'
+    document.getElementsByClassName('errada')[elemento[1]].style = 'background-color: red;'
+    document.getElementsByClassName('errada')[elemento[2]].style = 'background-color: red;'
+    document.getElementsByClassName('certa')[elemento[3]].style = 'background-color: green;'
 
-function avaliar3(resposta) {   
-    if(!resposta) {
-        erros = erros + 1
-    }
-    document.getElementsByClassName('errada3')[0].style = 'background-color: red;'
-    document.getElementsByClassName('errada3')[1].style = 'background-color: red;'
-    document.getElementsByClassName('errada3')[2].style = 'background-color: red;'
-    document.getElementsByClassName('certa3')[0].style = 'background-color: green;'
-}
-
-function avaliar4(resposta) {   
-    if(!resposta) {
-        erros = erros + 1
-    }
-    document.getElementsByClassName('errada4')[0].style = 'background-color: red;'
-    document.getElementsByClassName('errada4')[1].style = 'background-color: red;'
-    document.getElementsByClassName('errada4')[2].style = 'background-color: red;'
-    document.getElementsByClassName('certa4')[0].style = 'background-color: green;'
-    document.write('Você errou ' + erros+ ' Vezes')
+    if(pergunta === "ultima")
+        document.write('Você errou ' + erros+ ' Vezes')
 }
 
